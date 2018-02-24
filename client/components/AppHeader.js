953b5c78279cadba6cb7973b8cc4469603567547
Roshni Kutty <roshni.ajkutty@gmail.com>
2018-02-24 07:38:46 -0500

    added bootstrap

--- client/components/AppHeader.js
+++ client/components/AppHeader.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 let AppHeader = () => {
-    return(<h1>Link Shortener</h1>);
+    return (
+        <nav className="nav navbar-default">
+            <div className="navbar-header">
+                <a className="navbar-brand">ShortenMyLinks</a>
+            </div>
+        </nav>);
 }
 
 export default AppHeader;
