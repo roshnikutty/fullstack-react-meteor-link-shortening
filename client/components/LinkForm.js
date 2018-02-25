d0272e196a0656c07adfa257107d73b26c85ae6a
Roshni Kutty <roshni.ajkutty@gmail.com>
2018-02-25 14:50:29 -0500

    add graceful error handling

--- client/components/LinkForm.js
+++ client/components/LinkForm.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class LinkForm extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            error: ''
+        }
+    }
     handleSubmit(e) {
         e.preventDefault();
         // console.log(this.refs.userInpval.value);
-        Meteor.call('links.insert', this.refs.userInpval.value);
+        Meteor.call('links.insert', this.refs.userInpval.value, (error)=> {
+            if(error) {
+                this.setState({error: "Please enter a valid url"})
+            } else {
+                this.setState({error: ''});
+                this.refs.userInpval.value = ''
+            }
+        });
     }
     render() {
         return (
@@ -14,6 +27,7 @@ class LinkForm extends Component {
                     <input className="form-control" 
                         ref="userInpval"/>
                 </div>
+                <div className='text-danger'>{this.state.error}</div>
                 <button className="btn btn-primary">Shorten link</button>
             </form>
         );
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,10 +1,14 @@
 import { Mongo } from 'meteor/mongo';
-import { Meteor } from 'meteor/meteor';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 Meteor.methods({
     'links.insert': function(url){
-        console.log('attempting to save', url);
+        // validUrl.isUrl(url) returns url if valid; undefined if invalid
+        //check does not give any message if all is fine, else throws a JS error in console
+        //this error if it exists shows in Meteor.call of client/components/LinkForm callback 
+        check(url, Match.Where(url => validUrl.isUri(url)))
     }
-})
+});
 
 export const Links = new Mongo.Collection('links'); 
