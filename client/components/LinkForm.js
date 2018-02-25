a1a0864df1e843bcc19d38afda1f6b86a2f39dd7
Roshni Kutty <roshni.ajkutty@gmail.com>
2018-02-25 07:43:31 -0500

    remove meteor's insecure package

--- client/components/LinkForm.js
+++ client/components/LinkForm.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class LinkForm extends Component {
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.refs.userInpval.value);
+    }
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)}>
                 <div className="form-group">
                     <label>Link to shorten</label>
-                    <input className="form-control" />
+                    <input className="form-control" 
+                        ref="userInpval"/>
                 </div>
                 <button className="btn btn-primary">Shorten link</button>
             </form>
