672111d1b4d1e6d551875df3f9f2256a277ab2a5
Roshni Kutty <roshni.ajkutty@gmail.com>
2018-02-27 11:32:50 -0500

    added redirect logic

--- client/components/LinkList.js
+++ client/components/LinkList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { withTracker } from 'meteor/react-meteor-data';
-import { Links } from '../../imports/collections/links'
+// import { withTracker } from 'meteor/react-meteor-data';
+import { Links } from '../../imports/collections/links';
+import { createContainer } from 'meteor/react-meteor-data'
 
 class LinkList extends Component {
     renderRows() {
-        return this.props.links.map(link => {
+        let links = this.props.links.map(link => {
             const shortLink = `http://localhost:3000/${link.token}`
             return (
                 <tr key={link.token}>
@@ -16,26 +17,35 @@ class LinkList extends Component {
                 </tr>
             )
         })
+        return links;
     }
+
     render() {
-        return (<table className='table'>
-            <thead>
-                <tr>
-                    <th>URL</th>
-                    <th>Address</th>
-                    <th>Clicks</th>
-                </tr>
-            </thead>
-            <tbody>
-                {this.renderRows()}
-            </tbody>
-        </table>);
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>URL</th>
+                        <th>Address</th>
+                        <th>Clicks</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        );
     }
+
+    // export default withTracker(() => {
+    //     Meteor.subscribe('list');
+    //     return {
+    //         links: Links.find({}).fetch()
+    //     }
+    // })(LinkList);
 }
 
-export default withTracker(() => {
-    Meteor.subscribe('list');
-    return {
-        links: Links.find({}).fetch()
-    }
-})(LinkList);
+export default createContainer(() => {
+    Meteor.subscribe('links');
+    return { links: Links.find({}).fetch() };
+}, LinkList);
--- server/main.js
+++ server/main.js
@@ -1,8 +1,35 @@
 import { Links } from '../imports/collections/links';
+import { WebApps } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';  //inspect url and can be instructed to do diff. things
 
 
 Meteor.startup(() => {
-    Meteor.publish('links', function(){
-        return links.find({});
-    })
+    Meteor.publish('links', function () {
+        return Links.find({});
+    });
 });
+
+//Executed whenever user visits a route like localhost:3000/abcd
+function onRoute(req, res, next) {
+    const link = Links.findOne({ token: req.params.token });
+    if (link) {
+        //send response code 307 for redirect and
+        // assign Location with the long url found
+        Links.update(link, {$inc: {clicks: 1}}); //check mongo docs on Mongo modifier- increment click by 1
+        res.writeHead(307, { 'Location': link.url });
+        res.end();
+    }
+    else {
+        //let next middleware if it exists handle the request else it moves on to the React app
+        next();
+    }
+}
+
+//ConnectRoute creates a middleware that takes incoming http request and checks 
+//if it has /anything format and executes the callback
+
+const middleware = ConnectRoute(function (router) {
+    router.get('/:token', onRoute);
+});
+
+WebApp.connectHandlers.use(middleware);
